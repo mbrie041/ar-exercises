cd894242819c34c7c8a8e200c14052734416d38e
Mike <mbrie041@gmail.com>
2021-04-27 00:13:44 +0000

    exercise 2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,26 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+s1  = Store.new 
+s1.name = "Burnaby"
+s1.annual_revenue = 300000
+s1.mens_apparel = true
+s1.womens_apparel = true
+s1.save
+
+s2  = Store.new 
+s2.name = "Richmond"
+s2.annual_revenue = 1260000
+s2.mens_apparel = false
+s2.womens_apparel = true
+s2.save
+
+s3  = Store.new 
+s3.name = "Gastown"
+s3.annual_revenue = 190000
+s3.mens_apparel = true
+s3.womens_apparel = false
+s3.save
+
+puts "-----Store Count-----"
+puts Store.count(:all)
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,14 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+@store1.id
+
+@store1.name = "Ottawa"
+@store1.save
+puts "-----Store One's name-----"
+puts Store.find_by(id: 1).name
+
+
